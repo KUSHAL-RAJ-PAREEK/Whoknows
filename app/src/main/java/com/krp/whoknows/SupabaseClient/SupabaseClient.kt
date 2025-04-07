71a1e63eebae5a50535e937c377acfb1fb855ff6
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2025-04-07 15:00:34 +0530

    Update SupabaseClient.kt

--- app/src/main/java/com/krp/whoknows/SupabaseClient/SupabaseClient.kt
+++ app/src/main/java/com/krp/whoknows/SupabaseClient/SupabaseClient.kt
@@ -16,8 +16,8 @@ import io.ktor.client.plugins.auth.Auth
 @OptIn(SupabaseInternal::class)
 fun supabaseClient(): SupabaseClient {
     return createSupabaseClient(
-        supabaseUrl = "https://dtgatrenwhgxvicpbxre.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0Z2F0cmVud2hneHZpY3BieHJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1ODQ5MTMsImV4cCI6MjA1NzE2MDkxM30.2ZcSpw6klDiHtsg2PIQTh1VQXn5JkgG4NVw0gmivvCY"
+        supabaseUrl = "SUPABASE_URL",
+        supabaseKey = "API_KEY"
     ) {
         install(Storage)
     }
